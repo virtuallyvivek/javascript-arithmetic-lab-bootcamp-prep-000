ad28af75bd92fe887654a040e3e4d482d2d94b30
Catherine Straus <vnair611@gmail.com>
2020-06-21 05:02:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function decrement(n){
   return n-= 1
 }
 
-function makeInt(string){
-  return parseInt('string', 10)
+function makeInt(n){
+  return parseInt('n', 10)
 }
 
-function preserveDecimal(string){
-  return parseFloat('string')
+function preserveDecimal(n){
+  return parseFloat('n')
 }
